4fd78219dbd31492870f9292cf2160278ca3795c
Jeyaragu <45706920+Jeyaragu@users.noreply.github.com>
2019-12-08 22:55:19 +0530

    Loginpage
    08122019-LocalMachin

---
+++ Insert.php
@@ -0,0 +1,47 @@
+<?php
+
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
+$email=$_POST['email'];
+$employeeid=$_POST['emid'];
+$pwd=$_POST['pwd'];
+error_reporting(0);
+
+// echo $name."<br>";
+// echo $address."<br>";
+// echo $Email."<br>";
+// echo $number."<br>";
+// echo $state."<br>";
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+// Create connection
+$raghu =mysqli_connect($servername, $username, $password);
+
+// Check connection
+if ($raghu->connect_error) {
+    die("Connection failed: " . $raghu->connect_error);
+} 
+// else{
+// 	echo "DB connected successfully";
+// }
+else{
+$fname=mysqli_real_escape_string($raghu,$_REQUEST['fname']);
+$lname=mysqli_real_escape_string($raghu,$_REQUEST['lname']);
+$email=mysqli_real_escape_string($raghu,$_REQUEST['email']);
+$employeeid=mysqli_real_escape_string($raghu,$_REQUEST['emid']);
+$pwd=mysqli_real_escape_string($raghu,$_REQUEST['pwd']);
+$sql= "Insert into sample.employee_details (FNAME,LNAME,EMAIL_ID,E_ID,MOBILE_NO) values
+('$fname','$lname','$email','$employeeid','$pwd')";
+
+if(mysqli_query($raghu,$sql))
+{
+	echo "Record Inserted succesfully";
+}
+else{
+	echo "Error.Could not insert $sql".mysqli_error($raghu);
+}
+}
+?>
+
